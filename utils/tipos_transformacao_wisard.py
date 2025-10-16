6cdb7f52ee80093a6e228dba3191e00079a809f3
MarinaPiragibe <marinapiragibe@id.uff.br>
2025-10-16 12:48:27 -0300

    feat: implementação do wisard pkg

---
+++ utils/tipos_transformacao_wisard.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class TiposDeTransformacao(Enum):
+    BASICA = "basica"
+    ESCALA_DE_CINZA = "escala_de_cinza"
+    THRESHOLD_3 = "threshold_3"
+    THRESHOLD_31 = "threshold_31"
