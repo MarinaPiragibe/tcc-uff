e5f6a14c76ea08d0a157eaf90b6fd334765544e3
MarinaPiragibe <marinapiragibe@id.uff.br>
2025-10-31 14:34:46 -0300

    feat: extração de caracteristicas com metodos de visao computacional

--- extracao_caracteristicas.py
+++ extracao_caracteristicas.py
@@ -6,11 +6,11 @@ import torch
 from utils.dataset_utils import DatasetUtils
 from utils.enums.datasets_name_enum import DatasetName
 from utils.enums.tipos_transformacao_wisard import TiposDeTransformacao
-from old.fisher_vector import FisherVector, TipoDescritor
 from utils.logger import Logger
 from torch.utils.data import Subset
 
 from old.stride_hd import StrideHD
+from wisard.fisher_vector import FisherVector
 
 
 
@@ -18,7 +18,7 @@ args = {
 	"tamanho_lote": 32,
 	"dataset": DatasetName.CIFAR10,
 	"download_dataset": False,
-	"tipo_transformacao": TiposDeTransformacao.STRIDE_HD,
+	"tipo_transformacao": TiposDeTransformacao.FISHER_VECTOR,
 	"debug": False
 }
 args["data_execucao"] = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
@@ -51,39 +51,15 @@ test_loader = torch.utils.data.DataLoader(
 match(args['tipo_transformacao']):
 
 	case TiposDeTransformacao.STRIDE_HD:
-		stride_hd = StrideHD(window_size=(4,4), stride=3)
+		stride_hd = StrideHD(window_size=(2,2), stride=1)
 		stride_hd.executar_e_salvar(train_loader, test_loader, args['dataset'])
 
-	# case TiposDeTransformacao.FISHER_VECTOR:
-	# 	fv = FisherVector(
-	# 		train_loader=train_loader,
-	# 		test_loader=test_loader,
-	# 		dataset=args['dataset']
-	# 	)
+	case TiposDeTransformacao.FISHER_VECTOR:
+		fv = FisherVector(
+			train_set=dados_treino,
+			test_set=dados_teste,
+			args=args
+		)
+		fv.executar_e_salvar()
 
-	# 	logging.info(f"[INICIO] Execução do fisher vector com o descritor ORB")
-
-	# 	fv.executar_fisher_vector(
-	# 		tipo_descritor=TipoDescritor.ORB
-	# 	)
-
-	# 	logging.info(f"[FIM] Execução do fisher vector com o descritor ORB")
-
-
-	# 	logging.info(f"[INICIO] Execução do fisher vector com o descritor SIFT")
-
-	# 	fv.executar_fisher_vector(
-	# 		tipo_descritor=TipoDescritor.SIFT
-	# 	)
-
-	# 	logging.info(f"[FIM] Execução do fisher vector com o descritor SIFT")
-
-
-	# 	logging.info(f"[INICIO] Execução do fisher vector com o descritor SIFT COLORIDO")
-
-	# 	fv.executar_fisher_vector(
-	# 		tipo_descritor=TipoDescritor.SIFT_COLORIDO
-	# 	)
-
-	# 	logging.info(f"[FIM] Execução do fisher vector com o descritor SIFT COLORIDO")
-
+	
